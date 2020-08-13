e9f36e13a0b25fbf40fdf918e1d42a2dfa3ed7d1
Haris_Rehman <harisrehman155@gmail.com>
2020-08-14 03:39:43 +0500

    Update Child.js

--- src/Child.js
+++ src/Child.js
@@ -6,7 +6,7 @@ const Child = () => {
     let counterValue = useContext(counterContext)
     return (
         <div>
-            <h2>this is Child</h2>
+            <h2>This is Child</h2>
             <h2>Counter Value is :  {counterValue[0]}</h2>
 
             <button onClick={ () => { counterValue[1]( ++counterValue[0]) } }>
